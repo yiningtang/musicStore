043ca0bf43b3ef32b63d42b0a19a004e639dd196
Yining Tang <yiningtang@Yinings-MacBook-Pro.local>
2021-02-22 17:52:24 +1100

    initial commit

---
+++ apps/music-store/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'music-store-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'music-store';
+}
